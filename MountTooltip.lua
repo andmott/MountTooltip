a7c034ab5d2121e783508f74cc5c211387f7dc25
FuctUp <46389303+FuctUp@users.noreply.github.com>
2022-11-18 11:43:37 +0000

    Added in mount source to tooltip

--- MountTooltip.lua
+++ MountTooltip.lua
@@ -68,6 +68,9 @@ function MountTooltip.CheckAurasForMount(
 
     -- add mount info to the tooltip
     GameTooltip:AddLine(iconString .. mount.name);
+    GameTooltip:AddLine(mount.whereFrom);
+    GameTooltip:AddLine(mount.description);
+
 
     -- breaks the loop from AuraUtil because our function returns a value
     return true;
